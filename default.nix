2fd3f4d982cab6a3807e3a6c44d30273ecc5cea0
Enrico Zandomeni Borba <enricozb@gmail.com>
2022-11-18 22:06:16 -1000

    default.nix

--- default.nix
+++ default.nix
@@ -2,13 +2,13 @@
 
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "clip-rs";
-  version = "0.0.1";
+  version = "0.0.2";
 
   src = pkgs.fetchFromGitHub {
     owner = "enricozb";
     repo = "clip-rs";
     rev = "v${version}";
-    sha256 = "sha256-rWdhH5783rLMMDMzfOcpiD3+fpGoExgfDrum3rR6BCk=";
+    sha256 = "sha256-QLH6OJsU+9gf+NT2AOP7a4OxHyn9w4Wh+VQAjZNlsXo=";
   };
 
   cargoLock = { lockFile = ./Cargo.lock; };
