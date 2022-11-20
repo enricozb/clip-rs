351459eace862ad2c9596996991a8a345bee3b45
Enrico Zandomeni Borba <enricozb@gmail.com>
2022-11-19 20:04:47 -1000

    remove git commit from default.nix

--- default.nix
+++ default.nix
@@ -2,14 +2,7 @@
 
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "clip-rs";
-  version = "0.0.3";
-
-  src = pkgs.fetchFromGitHub {
-    owner = "enricozb";
-    repo = "clip-rs";
-    rev = "v${version}";
-    sha256 = "sha256-8Vmw8HYeMGSSyNSjiuRphZozNkCjgev3pzZvDwEs7f4=";
-  };
+  src = ./.;
 
   cargoLock = { lockFile = ./Cargo.lock; };
 
